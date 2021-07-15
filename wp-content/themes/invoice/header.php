e2015453efc0fd44a0fbb538b6613bf4fcf1f150
johnenrick <johnenrickplenos@gmail.com>
2021-07-15 21:17:39 +0800

    set up underscore.mem SASS

--- wp-content/themes/invoice/header.php
+++ wp-content/themes/invoice/header.php
@@ -25,24 +25,11 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'invoice' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header d-flex">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$invoice_description = get_bloginfo( 'description', 'display' );
-			if ( $invoice_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $invoice_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
