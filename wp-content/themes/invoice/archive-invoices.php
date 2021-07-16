f029023f327bc5c676c0f908990290d49db90fa3
johnenrick <johnenrickplenos@gmail.com>
2021-07-16 16:12:33 +0800

    design invoice page

--- wp-content/themes/invoice/archive-invoices.php
+++ wp-content/themes/invoice/archive-invoices.php
@@ -12,18 +12,60 @@ get_header();
 	<main id="primary" class="site-main p-4">
 		
 		<h1 class="page-header">Invoices</h1>
-		<div class="d-flex align-items-center justify-content-space-between">
+		<div class="d-flex align-items-center justify-content-space-between mb-2">
 			<div class="text-uppercase">
 				<span class="badge badge-primary2 mx-1">All</span>
-				<span class="text-secondary mx-1">Ongoing</span>
-				<span class="text-secondary mx-1">Verified</span>
-				<span class="text-secondary mx-1">Pending</span>
+				<span class="text-primary2 mx-1">Ongoing</span>
+				<span class="text-primary2 mx-1">Verified</span>
+				<span class="text-primary2 mx-1">Pending</span>
 			</div>
 			<div></div>
 			<button class="btn btn-warning">Mark As Paid</button>
 		</div>
+		<div>
+			<table class="table">
+				<thead class="bg-white">
+					<tr >
+						<th class="text-center"><input type="checkbox" /></th>
+						<th class="text-center">ID</th>
+						<th>Restaurant</th>
+						<th>Status</th>
+						<th>Start Date</th>
+						<th>End Date</th>
+						<th>Total</th>
+						<th>Fees</th>
+						<th>Transfer</th>
+						<th>Orders</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="text-center py-2 px-1"><input type="checkbox" /></td>
+						<td class="text-center py-2 px-1">#23212</td>
+						<td class="py-2 px-1">Jade Phoenix Buffet</td>
+						<td class="text-center py-2 px-1"><span class="badge badge-pill badge-secondary text-uppercase">Ongoing</span></td>
+						<td class="text-center py-2 px-1">16/08/2018</td>
+						<td class="text-center py-2 px-1">16/08/2018</td>
+						<td class="text-right py-2 px-1">HK$2.99</td>
+						<td class="text-right py-2 px-1">HK$2.99</td>
+						<td class="text-right py-2 px-1">HK$2.99</td>
+						<td class="text-right py-2 px-1">20</td>
+						<td>Download</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</main><!-- #main -->
-
+	<script type="application/javascript">
+		setTimeout(function(){
+			console.log(jQuery)
+			console.log($)
+		}, 4000)
+		
+	</script>
 <?php
 get_sidebar();
 get_footer();
+?>
+
